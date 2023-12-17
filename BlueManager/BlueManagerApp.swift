be7bf3925a8d8a507a41326cc5bbe8e9bbb70f15
Cattivole <cattivole@icloud.com>
2023-12-18 00:56:12 +0600

    Initial Commit

---
+++ BlueManager/BlueManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlueManagerApp.swift
+//  BlueManager
+//
+//  Created by Alexander Omelchuk on 18.12.23..
+//
+
+import SwiftUI
+
+@main
+struct BlueManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
